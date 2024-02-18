f12d6f6debe7b738dec59ed593808a21a8310f3d
kaygizzle Khangelani <mgoqikhangelani@gmail.com>
2024-02-18 15:36:11 +0000

    raise err

--- mean_var_std.py
+++ mean_var_std.py
@@ -3,6 +3,9 @@ import pprint
 import numpy as np
 
 def calculate(list):
+    if len(list) != 9:
+        raise ValueError('List must contain nine numbers.')
+    
     converted_list = np.array(list).reshape(3,3)
 
     calculations = {
